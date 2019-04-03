96613ffed23737ad4f503e50238fea36223b5ef5
Cara Ponzini <ponzini@gmail.com>
2019-04-02 21:03:41 -0700

    created and passed test to replace object plus test edits

--- test/index.test.js
+++ test/index.test.js
@@ -21,12 +21,27 @@ describe('create memory class', () => {
     expect(memory.findById(copy._id)).toEqual({ ...copy });
   });
 
+  test('returns object based on id', () => {
+    const memory = new MemoryDatabase();
+    const obj = { name: 'Superman' };
+    memory.create(obj);
+    expect(memory.findById(2)).toEqual(null);
+  });
+
   test('returns all objects in this.store', () => {
     const memory = new MemoryDatabase();
     const dog = memory.create({ name: 'Ana Dogg' });
     const kitty = memory.create({ name: 'Fat Kitty' });
     expect(memory.find()).toEqual([{ ...dog }, { ...kitty }]);
   });
+
+  test('finds and replaces object by id', () => {
+    const memory = new MemoryDatabase();
+    const copy = memory.create({ name: 'Superman' });
+    let id = copy._id;
+    expect(memory.findByIdReplace(id, { name: 'Batman' })).toEqual({ _id:expect.any(String), name: 'Batman' });
+  });
+
 
 
 });
