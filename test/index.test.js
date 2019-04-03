ab4626d3643fdc3fba6cd3f2382d36aeb0e09ac2
Cara Ponzini <ponzini@gmail.com>
2019-04-02 20:20:51 -0700

    test and create method added

---
+++ test/index.test.js
@@ -0,0 +1,16 @@
+
+const { MemoryDatabase } = require('../lib/index.js');
+
+describe('create memory class', () => {
+  it('create empty storage', () => {
+    const memory = new MemoryDatabase();
+    expect(memory.store).toEqual({});
+  });
+
+  it('adds an name obj to the store', () => {
+    const memory = new MemoryDatabase();
+    const obj = { name: 'Superman' };
+    const copy = memory.create(obj);
+    expect(copy.name).toEqual('Superman');
+  });
+});
