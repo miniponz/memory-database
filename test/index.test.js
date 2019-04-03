0e4154565e4b44048468fc6425a1ceecd4cbb2f0
Cara Ponzini <ponzini@gmail.com>
2019-04-02 21:47:16 -0700

    set memory before tests

--- test/index.test.js
+++ test/index.test.js
@@ -2,55 +2,52 @@
 const { MemoryDatabase } = require('../lib/index.js');
 
 describe('create memory class', () => {
+  let memory = null;
+  beforeEach(() => {
+    memory = new MemoryDatabase();
+  });
+
   it('create empty storage', () => {
-    const memory = new MemoryDatabase();
     expect(memory.store).toEqual({});
   });
 
   it('adds an name obj to the store', () => {
-    const memory = new MemoryDatabase();
     const obj = { name: 'Superman' };
     const copy = memory.create(obj);
     expect(copy.name).toEqual('Superman');
   });
 
   test('returns object based on id', () => {
-    const memory = new MemoryDatabase();
     const obj = { name: 'Superman' };
     const copy = memory.create(obj);
     expect(memory.findById(copy._id)).toEqual({ ...copy });
   });
 
   test('returns object based on id', () => {
-    const memory = new MemoryDatabase();
     const obj = { name: 'Superman' };
     memory.create(obj);
     expect(memory.findById(2)).toEqual(null);
   });
 
   test('returns all objects in this.store', () => {
-    const memory = new MemoryDatabase();
     const dog = memory.create({ name: 'Ana Dogg' });
     const kitty = memory.create({ name: 'Fat Kitty' });
     expect(memory.find()).toEqual([{ ...dog }, { ...kitty }]);
   });
 
   test('finds and replaces object by id', () => {
-    const memory = new MemoryDatabase();
     const copy = memory.create({ name: 'Superman' });
     let id = copy._id;
     expect(memory.findByIdReplace(id, { name: 'Batman' })).toEqual({ _id:expect.any(String), name: 'Batman' });
   });
 
   test('find object by id and delete', () => {
-    const memory = new MemoryDatabase();
     const copy = memory.create({ name: 'yer mom' });
     let id = copy._id;
     expect(memory.findByIdDelete(id)).toEqual({ ...copy });
   });
 
   test('drop clears storage', () => {
-    const memory = new MemoryDatabase();
     memory.create({ name: 'hungry hippo' });
     memory.drop();
     expect(memory.store).toEqual({});
